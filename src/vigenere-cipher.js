336f089ff5d53b342c941e8db0203585d07f6712
github-dns147 <rarden@ya.ru>
2022-10-14 23:11:19 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,122 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(state) {
+    this.state = state;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const lowerMessage = message.toLowerCase();
+    const lowerKey = key.toLowerCase();
+    let newKey = '';
+    let space = {};
+    let resultArr = [];
+    const reg = new RegExp('^[a-zA-Z]+$');
+
+    while (newKey.length < lowerMessage.length) {
+      newKey += lowerKey;
+    }
+
+    for (let index = 0; index < lowerMessage.length; index += 1) {
+      const pos = lowerMessage[index].charCodeAt(0) - 97;
+
+      if (!reg.test(lowerMessage[index])) {
+        space[index] = lowerMessage[index];
+      } 
+      
+      if (pos < 0) {
+        space[index] = lowerMessage[index];
+      }
+    }
+
+    let newMessage = lowerMessage;
+
+    for (let k in space) {
+      newMessage = newMessage.split(space[k]).join('');
+    }
+     
+    for (let index = 0; index < newMessage.length; index += 1) {
+      const posLetterMessage = newMessage[index].charCodeAt(0) - 97;
+      
+      if (posLetterMessage >= 0) {
+        const posLetterKey = newKey[index].charCodeAt(0) - 97;
+        const newPos = (posLetterMessage + posLetterKey) % 26;
+
+        resultArr.push(String.fromCharCode(newPos + 97));
+      }
+    }
+
+    for (let k in space) {
+      const del = resultArr.splice(Number(k));
+      resultArr[Number(k)] = space[k];
+      resultArr = resultArr.concat(del);
+    }
+
+    if (this.state === false) {
+      resultArr.reverse();
+    }
+
+    return resultArr.join('').toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    const lowerMessage = message.toLowerCase();
+    const lowerKey = key.toLowerCase();
+    let newKey = '';
+    let space = {};
+    let resultArr = [];
+    const reg = new RegExp('^[a-zA-Z]+$');
+
+    while (newKey.length < message.length) {
+      newKey += lowerKey;
+    }
+
+    for (let index = 0; index < lowerMessage.length; index += 1) {
+      const pos = lowerMessage[index].charCodeAt(0) - 97;
+
+      if (!reg.test(lowerMessage[index])) {
+        space[index] = lowerMessage[index];
+      } else if (pos < 0) {
+        space[index] = lowerMessage[index];
+      }
+    }
+
+    let newMessage = lowerMessage;
+
+    for (let k in space) {
+      newMessage = newMessage.split(space[k]).join('');
+    }
+    
+    for (let index = 0; index < newMessage.length; index += 1) {
+      const posLetterMessage = newMessage[index].charCodeAt(0) - 97;
+    
+      if (posLetterMessage >= 0) {
+        const posLetterKey = newKey[index].charCodeAt(0) - 97;
+        const newPos = (26 + (posLetterMessage - posLetterKey)) % 26;
+
+        resultArr.push(String.fromCharCode(newPos + 97));
+      }
+    }
+
+    for (let k in space) {
+      const del = resultArr.splice(Number(k));
+      resultArr[Number(k)] = space[k];
+      resultArr = resultArr.concat(del);
+    }
+
+    if (this.state === false) {
+      resultArr.reverse();
+    } 
+
+    return resultArr.join('').toUpperCase();
   }
 }
 
